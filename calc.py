895daff03e541f1a1d9949d21a8f3a4ce7893b49
YuvamGurukul <156825575+YuvamGurukul@users.noreply.github.com>
2024-02-01 21:06:51 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,7 @@
 print('Hi this is a simple subtract')
 first=input('Enter the first number')
 second=input('Enter second number')
+print('The result is ', first+second)
 print('The result is ', first-second)
+print('The result is ', first*second)
+print('The result is ', first/second)
